dd45bb1ff56a4f05ce3c2e0209b760e954257e06
AnnaMakaruk <anna.makaruk@parx.com>
2022-06-24 14:28:17 +0300

    ghjkl

--- blocksdk.js
+++ blocksdk.js
@@ -7,8 +7,8 @@
 
 var SDK = function (whitelistOverride, sslOverride) {
 
-	// the custom block should verify it is being called from ()()()(()())
-	// the marketing cloud)))
+	// the custom block should verify it is being called from ()()
+	// the marketing cloud
 	this._validateOrigin = function (origin) {
 		// Make sure to escape periods since these strings are used in a regular expression
 		var allowedDomains = whitelistOverride || ['marketingcloudapps\\.com', 'blocktester\\.herokuapp\\.com'];
